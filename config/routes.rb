ac785b8b151dc0e801b2200d3e4b786f0d81d304
masaki-fujita <stalemate-pg@i.softbank.jp>
2019-04-27 10:36:23 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+  get 'microposts/destory'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'toppages#index'
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
